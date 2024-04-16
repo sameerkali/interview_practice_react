b994394b61eddea993d8654a262fdda54a34d6e9
Sameer Faridi <s.faridi007@gmail.com>
2024-04-16 12:52:39 +0530

    memoization done

--- src/Todo6.jsx
+++ src/Todo6.jsx
@@ -1,20 +1,45 @@
-import { useState } from "react";
+import { memo, useState, useMemo, useCallback } from "react";
 import { Link } from "react-router-dom";
+
+const NewComp = ({ params, onClick }) => { // i add onlick for breaking useMemo
+  console.log("NewComp rerendered", params.color);
+  return (
+    <div
+      style={{ margin: 2, width: 70, height: 70, backgroundColor: params.color }} 
+    />
+  );
+};
+
+const MemoizedNewComp = memo(NewComp);  // shallow comparison as default 
+
+//  return prev.params.color === next.params.color
+// const MemoizedNewComp = memo(NewComp, (prev, next) => { // if you want to deep comparison you have to pass a callback function like this 
+
+// }); 
+
 
 const Todo6 = () => {
   const [todo, setTodo] = useState([{ name: "Learn React" }]);
   const [onlyTodo, setOnlyTodo] = useState("");
+  const [color, setColor] =useState('red')
+  
+  // using useMemo
+  const params = useMemo(() => ({color}),[color])
+  // usign useCallback
+  const onClick = useCallback(() =>{}, [])
 
   const addTodo = () => {
     if (onlyTodo.trim() !== "") {
       setTodo([...todo, { name: onlyTodo }]);
       setOnlyTodo("");
+      setColor('green')
     }
   };
 
-  const removeTodo = (index) => {
+  const removeTodo = index => {
     setTodo(todo.filter((allValues, idx) => idx !== index));
   };
+
 
   return (
     <div className="max-w-2xl mx-auto my-8">
@@ -46,12 +71,13 @@ const Todo6 = () => {
         </Link>
       </div>
       <h1 className="text-5xl font-bold my-4">Todo List 06</h1>
+      <MemoizedNewComp params={params} onClick={onClick} />
       <div className="flex items-center space-x-4 my-4">
         <input
           type="text"
           value={onlyTodo}
           placeholder="Add Todo"
-          onChange={(e) => setOnlyTodo(e.target.value)}
+          onChange={e => setOnlyTodo(e.target.value)}
           className="border border-gray-300 px-3 py-2 w-64 focus:outline-none"
         />
         <button
@@ -63,7 +89,7 @@ const Todo6 = () => {
       </div>
       <div>
         <ol>
-          {todo.map((todo, index) => (
+          {todo.map((todo, index) =>
             <div key={index} className="mb-2">
               <li className="flex justify-between items-center border-b border-gray-300 py-2">
                 {todo.name}
@@ -75,7 +101,7 @@ const Todo6 = () => {
                 </button>
               </li>
             </div>
-          ))}
+          )}
         </ol>
       </div>
     </div>
