aec3558b1b5e9b60a6617b439a1d42d1b719e66c
Sameer Faridi <s.faridi007@gmail.com>
2024-04-17 12:39:01 +0530

    harshit bhaiya rocks

--- src/liftingStateUp/Child.jsx
+++ src/liftingStateUp/Child.jsx
@@ -1,31 +1,31 @@
 
-// import React from 'react';
+import React from 'react';
 
-// const ChildComponent = ({ inputValue, onInputChange }) => {
-//   console.log(onInputChange,"onInputChange")
-//   // onInputChange('harshit')
-//   const name = 'sameer'
-//   onInputChange(name)
-//   return (
-//     <div>
-//       <label>Input:</label>
-//       <input type="text" value={inputValue} onChange={(e)=> onInputChange(e.target.value)} />
+const ChildComponent = ({ inputValue, onInputChange }) => {
+  console.log(onInputChange,"onInputChange")
+  // onInputChange('harshit')
+  const name = 'sameer'
+  onInputChange(name)
+  return (
+    <div>
+      <label>Input:</label>
+      <input type="text" value={inputValue} onChange={(e)=> onInputChange(e.target.value)} />
 
-//     </div>
-//   );
-// };
-
-// export default ChildComponent;
-
-
-// question ask by harshit bhaiya 
-const shape = {
-  radius: 10,
-  diameter() {
-    return this.radius * 2;
-  },
-  perimeter: () => 2 * Math.PI * this.radius,
+    </div>
+  );
 };
 
-console.log(shape.diameter()); //20
-console.log(shape.perimeter()); // NAN || undefined (because 'this' is not accessable in arrow functions) and undefined*number = undefined 
+export default ChildComponent;
+
+
+// // question ask by harshit bhaiya 
+// const shape = {
+//   radius: 10,
+//   diameter() {
+//     return this.radius * 2;
+//   },
+//   perimeter: () => 2 * Math.PI * this.radius,
+// };
+
+// console.log(shape.diameter()); //20
+// console.log(shape.perimeter()); // NAN || undefined (because 'this' is not accessable in arrow functions) and undefined*number = undefined 
