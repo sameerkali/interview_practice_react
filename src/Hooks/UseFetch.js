55d8080ae8bbcfeb93770f5c8571e5c9b9bad437
Sameer Faridi <s.faridi007@gmail.com>
2024-08-19 22:28:46 +0530

    create a fetch custom hook

--- src/Hooks/UseFetch.js
+++ src/Hooks/UseFetch.js
@@ -1,18 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-const UseFetch = (url) => {
-    const [data, setData] = useState([]);
+const UseFetch = url => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const getData = async() => {
-        const apidata = await fetch(url)
-        const dataApi = await apidata.json(url)
-        setData(dataApi)
+  const getData = async () => {
+    try {
+      const apidata = await fetch(url);
+      const dataApi = await apidata.json();
+      setData(dataApi);
+      setLoading(false);
+    } catch (err) {
+      console.log("OOPS - Error fetching:", err);
+      setLoading(true);
     }
-    useEffect(() =>{
-        getData()
-    }, [])
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
-  return data
-}
+  return { data, loading };
+};
 
-export default UseFetch
+export default UseFetch;
--- src/Todolatest.jsx
+++ src/Todolatest.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import UseFetch from "./Hooks/UseFetch";
 
 const Todolatest = () => {
-  const [todo, setTodo] = useState([]);
-  const [myTodo, setMyTodo] = useState([]);
+ const [data, loading] = UseFetch("https://jsonplaceholder.typicode.com/todos")
+ console.log(data, "from custom hook")
+  // const [todo, setTodo] = useState([]);
+  // const [myTodo, setMyTodo] = useState([]);
 //   console.log("----", todo, "----");
 
-  const fetchTodo = async () => {
-    let result = await fetch("https://jsonplaceholder.typicode.com/todos");
-    let data = await result.json();
-    setTodo(data);
-  };
+  // const fetchTodo = async () => {
+  //   let result = await fetch("https://jsonplaceholder.typicode.com/todos");
+  //   let data = await result.json();
+  //   setTodo(data);
+  // };
   //   const addTodo = async () => {
   //     let postTodo = {
   //       completed: true,
@@ -29,28 +32,28 @@ const Todolatest = () => {
   //     console.log(data);
   //   };
 
-  useEffect(() => {
-    fetchTodo();
-  }, []);
-  const handleChange = (e) => {
-    console.log(e)
-    setMyTodo(e)
-  }
+  // useEffect(() => {
+  //   fetchTodo();
+  // }, []);
+  // const handleChange = (e) => {
+  //   console.log(e)
+  //   setMyTodo(e)
+  // }
 
   return (
     <div>
       <div>Todo List</div>
       <div className="flex ">
-        <input type="text" onChange={(e)=> handleChange(e.target.value)}  placeholder="Enter your Todo Here" />
+        <input type="text" /*onChange={(e)=> handleChange(e.target.value)}*/ placeholder="Enter your Todo Here" />
         <button onClick={() => {}}>Add Todo</button>
       </div>
-      {todo.slice(0, 20).map(todo =>
+      {/* {todo.slice(0, 20).map(todo =>
         <ul key={todo.id}>
           <li>
             {todo.title}
           </li>
         </ul>
-      )}
+      )} */}
     </div>
   );
 };
